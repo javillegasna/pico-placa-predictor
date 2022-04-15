7c37da27737c380f92d2476f8e4ce38aaa2ce667
Javier Villegas <javillegasna@gmail.com>
2022-04-15 15:43:59 -0500

    adding test and workflow

--- src/App/App.test.tsx
+++ src/App/App.test.tsx
@@ -1,9 +1,33 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders title", () => {
   render(<App />);
-  const linkElement = screen.getByText("Pico & Placa");
-  expect(linkElement).toBeInTheDocument();
+  const h1Element = screen.getByText("Pico & Placa");
+  expect(h1Element).toBeInTheDocument();
+});
+
+test("render form", () => {
+  render(<App />);
+  const formElement = screen.getByText("Plate Number");
+  expect(formElement).toBeInTheDocument();
+});
+
+test("If button click with empty plate or date prediction will be hidden", () => {
+  render(<App />);
+  const buttonElement = screen.getByText("Predict");
+  fireEvent.click(buttonElement);
+  const cardElement = screen.queryByText(/The plates numbers/i);
+  expect(cardElement).not.toBeInTheDocument();
+});
+
+test("When plate input have a valid plate card will be render", () => {
+  render(<App />);
+  const buttonElement = screen.getByText("Predict");
+  const inputPlate = screen.getByDisplayValue("");
+  fireEvent.change(inputPlate, { target: { value: "TBD5695"}});
+  fireEvent.click(buttonElement);
+  const cardElement = screen.getByText(/The plates numbers/i)
+  expect(cardElement).toBeInTheDocument();
 });
